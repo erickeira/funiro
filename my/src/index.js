1d9a3af9933b8e3bbebe2851b8289f9c18c5be96
erickola <erick.almeida.ricardo@gmail.com>
2021-10-05 18:04:01 -0300

    Slider

--- my/src/index.js
+++ my/src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Topo from './componentes/topo'
 import Rodape from './componentes/rodape'
+import Slider from './componentes/slider'
 import './index.css'
 
 ReactDOM.render(
   <div>
     <Topo></Topo>
+    <Slider></Slider>
     <Rodape></Rodape>
   </div>,
 
